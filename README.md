0f3a858b7ba9fca221cf93d6c1270a7ec0397fc9
Abiodun Azeez <iamhabbeboy@gmail.com>
2022-11-10 17:33:47 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# electron-vite-vue
+
+🥳 Really simple `Electron` + `Vue` + `Vite` boilerplate.
+
+- Set a cron job task when a button is clicked
+- Set date to cron
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+</script>
+
+<template>
+<h1>Worship</h1>
+<b>Selected directory: {{path}} </b>
+<input type="file" @change="openDialog" />
+<label>Set time</label>
+<input type="datetime-local" />
+<button @click="playSong">Play</button>  
+<button @click="handleSchedule">Schedule </button>  
+</template>
+
+<script lang="ts">
+import { ref, computed} from 'vue'
+const Store = require('electron-store');
+
+export default {
+  setup() {
+    const store = new Store();
+    const path = computed(() => store.get('selected-directory'))
+
+    const openDialog = (e: any) => {
+      e.preventDefault()
+      window.postMessage({
+        type: 'select-dirs'
+      })
+    }
+
+    const playSong = () => {
+      let audio = new Audio('./song.mp3')
+      audio.play()
+    }
+
+    const handleSchedule = () => {
+      
+    }
+
+    return {
+      path,
+      playSong,
+      openDialog,
+      handleSchedule,
+    }
+  }
+}
+</script>
+
